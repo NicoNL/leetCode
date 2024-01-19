8a235e5f3f61d7f283f486ae9bf45e94b5a06194
NicoNL <a0t4zr@inf.elte.hu>
2024-01-18 23:13:43 -0500

    RemoveElement solution

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RemoveElement
+{
+    class Program
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int[] nums2 = new int[nums.Length];
+            int cnt = 0;
+            foreach (int num in nums)
+            {
+                if (num != val)
+                {
+                    nums2[cnt] = num;
+                    cnt++;
+                }
+            }
+            for(int i = 0; i < nums2.Length;i++)
+            {
+                nums[i] = nums2[i];
+            }
+            return cnt;
+        }
+    }
+}
