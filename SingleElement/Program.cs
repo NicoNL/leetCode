308fc163c8bf07437728486ea7033dddb49675b5
NicoNL <a0t4zr@inf.elte.hu>
2024-01-21 00:07:27 -0500

    Bad Solution of SingleElement Exercise

---
+++ SingleElement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace SingleElement
+{
+    class Programm
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int cnt = 0;
+            int single = 0;
+            for(int i=0; i<nums.Length; i++)
+            {
+
+                for(int j=0;j<nums.Length; j++) 
+                {
+                    if (nums[i] == nums[j] && i != j)
+                    {
+                        cnt = 0;
+                        break;
+                    }
+                    else
+                    {
+                        single = nums[i];
+                        cnt++;
+                    }
+                }
+                if(cnt == nums.Length )
+                {
+                    break;
+
+                }
+            }
+            return single;
+        }
+    }
+}
