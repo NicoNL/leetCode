e9085c9550531748deb2477dcce782e3048d2a3e
NicoNL <a0t4zr@inf.elte.hu>
2024-01-21 21:29:13 -0500

    SearchInsert exercise added

---
+++ SearchInsert/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+
+namespace SearchInsert
+{
+    class Programm
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if(nums.Contains(target)) 
+            {
+                int index = 0;
+                for(int i = 0; i < nums.Length; i++) 
+                {
+                    if (nums[i] == target)
+                    {
+                        index = i;
+                    }
+                }
+                return index;
+            }
+            else
+            {
+                if(target > nums.Max())
+                {
+                    return nums.Length;
+
+                }
+                if(target < nums.Min()) 
+                {
+                    return 0;
+                }
+                int index = 0;
+                for(int i = 0; i < nums.Length; ++i) 
+                {
+                    if ((i+1) < nums.Length && nums[i] < target && nums[i+1] > target) 
+                    {
+                       index = i;
+                        break;
+                    }
+                }
+                return index;
+            }
+        }
+    }
+}
