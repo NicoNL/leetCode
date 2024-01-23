42a9f0b191ee1746808d5abde228ca00e875f26d
NicoNL <a0t4zr@inf.elte.hu>
2024-01-22 22:52:29 -0500

    ContainsDuplicate solution added

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ContainsDuplicate
+{
+    class Programm
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            if (set.Count < nums.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
