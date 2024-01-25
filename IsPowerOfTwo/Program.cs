0f02ac47114580ec2cce45ec6f931924465c903e
NicoNL <a0t4zr@inf.elte.hu>
2024-01-24 21:27:31 -0500

    IsPowerOfTwo and RemoveDuplicate exercises added

---
+++ IsPowerOfTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IsPowerOfTwo
+{
+    //SOLUTION BASED ON KeerthiVarmanK's phyton solution
+    class Programm
+    {
+        public class Solution
+        {
+            public bool IsPowerOfTwo(int n)
+            {
+                if (n == 1)
+                {
+                    return true;
+                }
+                else if (n == 0 || (n % 2) != 0)
+                {
+                    return false;
+                }
+                return IsPowerOfTwo(n / 2);
+            }
+        }
+    }
+}
---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int[] tempArr = new int[nums.Length];
+            int cnt = 0;
+            bool zero = true;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i + 1 < nums.Length && nums[i] == nums[i + 1] && !(tempArr.Contains(nums[i])))
+                {
+                    tempArr[cnt] = nums[i];
+                    cnt++;
+                }
+                else if (!(tempArr.Contains(nums[i])))
+                {
+                    tempArr[cnt] = nums[i];
+                    cnt++;
+                }
+                else if (nums[i] == 0 && zero)
+                {
+                    tempArr[cnt] = 0;
+                    cnt++;
+                    zero = false;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = tempArr[i];
+            }
+            return cnt;
+        }
+    }
+}
