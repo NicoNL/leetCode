95cab3e2046fe3218f5f1220d1491710c7d700f6
NicoNL <a0t4zr@inf.elte.hu>
2024-01-25 21:57:22 -0500

    BesTime exercise added

---
+++ BestTime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BestTime
+{
+    class Program
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 1)
+            {
+                return 0;
+            }
+            int index1 = 0;
+            int diff = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int tempDiff = prices[i] - prices[index1];
+                if (prices[index1] > prices[i])
+                {
+                    index1 = i;
+                }
+                else if (tempDiff > diff)
+                {
+                    diff = tempDiff;
+                }
+            }
+            return diff;
+        }
+    }
+}
