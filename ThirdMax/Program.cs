a5bc60cefcbe56ea9816d2187a9b1faaa22947ec
NicoNL <a0t4zr@inf.elte.hu>
2024-01-27 18:15:47 -0500

    ThirdMax exercise added

---
+++ ThirdMax/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ThirdMax
+{
+    class Program
+    {
+        public int ThirdMax(int[] nums)
+        {
+            Array.Sort(nums);
+            int max3 = 0;
+            int max2 = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int max = nums.Max();
+                if (nums[i] < max)
+                {
+                    max2 = nums[i]; 
+                }
+                else if (nums[i] == max)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < max2)
+                {
+                    max3 = nums[i];
+                }
+                else if (nums[i] == max2)
+                {
+                    break;
+                }
+            }
+            if(max3 == 0 && nums.Contains(0))
+            {
+                return max3;
+            }
+            if(max3 == 0) 
+            {
+                max3 = max2;
+                return nums.Max();
+            }
+            return max2;
+        }
+   
+    }
+}
