993c0751f5c1b108feb617f65c94c36cb78c2e7e
NicoNL <a0t4zr@inf.elte.hu>
2024-01-26 23:09:28 -0500

    AddDigtis exercise added

---
+++ AddDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AddDigits
+{
+    class Program
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10)
+            {
+                return num;
+            }
+            else
+            {
+                string strNum = num.ToString();
+                int sum = 0;
+                for(int i = 0; i < strNum.Length; i++) 
+                {
+                    sum += Convert.ToInt32(strNum[i]) - '0';
+                }
+                return AddDigits(sum);
+            }
+        }
+    }
+}
